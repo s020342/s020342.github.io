5de470f5089dcb1a5187c90d5e083ed163bafaad
清风飞过 <42401721+s020342@users.noreply.github.com>
2021-09-29 16:08:40 +0800

    Update index.js

--- algorithm/js/index.js
+++ algorithm/js/index.js
@@ -1,4 +1,4 @@
-﻿var arr = "2019-05-29 18:06:00".split(/[- :]/);//改这里
+﻿var arr = "2021-09-23 21:03:00".split(/[- :]/);//改这里
 var box = document.getElementsByClassName('textCon')[0];
 function init() {
     var width = box.offsetWidth;
